6e1e97f0ea37cf59d1c7b762f68c41a9287f448b
Keenan1300 <hamilt25@sheridancollege.ca>
2026-03-16 17:20:09 -0400

    I didnt push my initial changes :(

---
+++ Assets/Scripts/MoveToPointAT.cs
@@ -0,0 +1,71 @@
+
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions
+{
+
+    public class MoveToPointAT : ActionTask
+    {
+        public Transform targetPoint;
+        public BBParameter<float> speed;
+        public BBParameter<float> arrivalDistance;
+
+        public float detectionDistance;
+        public Transform enemyTransform;
+
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit()
+        {
+            return null;
+        }
+
+        //This is called once each time the task is enabled.
+        //Call EndAction() to mark the action as finished, either in success or failure.
+        //EndAction can be called from anywhere.
+        protected override void OnExecute()
+        {
+
+        }
+
+        //Called once per frame while the action is active.
+        protected override void OnUpdate()
+        {
+            float distanceToEnemy = Vector3.Distance(agent.transform.position, enemyTransform.position);
+            if (distanceToEnemy < detectionDistance)
+            {
+                EndAction(false);
+                return;
+            }
+
+
+            Vector3 directionToMove = targetPoint.position - agent.transform.position;
+            agent.transform.position += directionToMove.normalized * speed.value * Time.deltaTime;
+
+            float distanceToTarget = Vector3.Distance(agent.transform.position, targetPoint.position);
+            if (distanceToTarget < arrivalDistance.value)
+            {
+                EndAction(true);
+            }
+
+
+
+        }
+
+        //Called when the task is disabled.
+        protected override void OnStop()
+        {
+
+        }
+
+        //Called when the task is paused.
+        protected override void OnPause()
+        {
+
+        }
+    }
+}
